64b6c02c0de6701439addc5e67cd361d7f864131
Damian Escobedo <damian050697@gmail.com>
2026-06-13 09:24:09 -0600

    Remove output from previous expressions by line

--- main.c
+++ main.c
@@ -1,20 +1,72 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 /*
  * TODO list:
  *  - after every loop remove the last loop output so it only shows
- *  the output from new expressions
+ *  the output from new expressions (this is almost done, just need to remove
+ *  last output by character instead of by line, because it remove output
+ *  that it shouldn't if the last output didn't have a break-line)
  *
- *  - if the compliation fails remove all input added during that loop from the file
+ *  - add ability to use arrow up and arrow down to check history and
+ *  left arrow and right arrow to edit the text
  *
- *  - add ability to use arrow up and arrow down to check history
- *
- *  - if the users adds an expresion that returns a value but they don't catch the value
+ *  - if the users adds an expression that returns a value but they don't catch the value
  *  we should print that return value
+ *
+ *  - add ability do edit history/current backing file
  */
+
+static int lastOutputLength = 0;
+
+typedef struct {
+    char buffer[256];
+} OutputLine;
+
+bool compileAndRun(char *tempFilePath) {
+    char sysCall[256] = "gcc ";
+    strcat(sysCall, tempFilePath);
+    strcat(sysCall, " -o repl 2>&1 && ./repl 2>&1 ");
+    FILE *pipe = popen(sysCall, "r");
+
+    OutputLine outputLine;
+    OutputLine allLines[256];
+    int lineCount = 0;
+    while (fgets(outputLine.buffer, sizeof(outputLine.buffer), pipe)) {
+        allLines[lineCount++] = outputLine;
+    }
+
+    int status = pclose(pipe);
+    if (WIFEXITED(status)) { // process terminated normally (did not crash)
+        int exitCode = WEXITSTATUS(status);
+        if (exitCode == 0) {
+            for (int i = lastOutputLength; i < lineCount; ++i) {
+                printf("%s\n", allLines[i].buffer);
+            }
+            //TODO: figure out a way to do this without removing output if
+            //the last output didn't have a break line on it's last line
+            //probably need to do this by character instead of line
+            lastOutputLength = lineCount;
+        } else {
+            // gcc return non zero exit code (could also be the users program)
+
+            // Print all output to show complilation errors
+            for (int i = 0; i < lineCount; ++i) {
+                printf("%s", allLines[i].buffer);
+            }
+            printf("gcc exit code non-zero\n");
+            return 0;
+        }
+    } else {
+        //TODO: Log
+        return 0;
+    }
+
+    return 1;
+}
 
 int main() {
     char *tempFilePath = "./temp.c";
@@ -27,7 +79,6 @@ int main() {
 
     fprintf(codeFile, "#include <stdio.h>\nint main() {\n");
     char lastExpression[256];
-    int lastOuputLength = 0;
     printf("C REPL: Enter an expression...\n");
     for (;;) {
         printf(": ");
@@ -42,15 +93,12 @@ int main() {
         fprintf(codeFile, "%s", "}\n");
         fflush(codeFile);
 
-        char sysCall[256] = "gcc ";
-        strcat(sysCall, tempFilePath);
-        strcat(sysCall, " -o repl && ./repl");
-        int gccReturnCode = system(sysCall);
-        if (gccReturnCode != 0) {
+        if (!compileAndRun(tempFilePath)) {
             ftruncate(fileno(codeFile), ptrBeforeNewLine);
             fseek(codeFile, ptrBeforeNewLine, SEEK_SET);
             continue;
         }
+
         printf("\n");
 
         // Remove "}\n for next loop"
